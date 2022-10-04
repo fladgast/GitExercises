1d5777150ad7b35feede34cfe0d21a10c6f7fe82
Emil <Emil.Johansson@awacademy.training>
2022-10-04 11:20:48 +0200

    Updated print statement for GitHub

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! Change 3");
         System.out.println("Another change");
+        System.out.println("Changes, hello GitHub!");
     }
 }
