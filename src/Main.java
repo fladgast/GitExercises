817bba3486ce7dc23a2c09d9d7de7bbca839daad
Emil <Emil.Johansson@awacademy.training>
2022-10-04 11:32:19 +0200

    Added another print statement about the new feature

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Another change");
         System.out.println("Changes, hello GitHub!");
         System.out.println("New changes, amazing!");
+        System.out.println("This is a new feature");
     }
 }
